113af926b67dea1ca16623a4402a8a9d3ccc8aa9
Flor <mariaflorenciaperezd@gmail.com>
2025-07-31 12:55:37 +0200

    resume

--- src/paginas/Resume.jsx
+++ src/paginas/Resume.jsx
@@ -6,7 +6,7 @@ import { studies, experiences } from '../data/resume.js';
 function Resume() {
   const laboralExprience = experiences.map((experience) => (
     <div key={experience.id} className="resume-card">
-      <h2>{experience.title}</h2>
+      <h3>{experience.title}</h3>
       <p>{experience.company}</p>
       <p>{experience.date}</p>
     </div>
@@ -19,20 +19,24 @@ function Resume() {
     </div>
   ));
   return (
-    <div className="resume">
-      <h1>Welcome to my Resume Page</h1>
-      <p>A little bit of me</p>
+   <>
+     <div className="sticky-links">
+            <Link to="/" className="right-link">Go to Home</Link>
+            <Link className="left-link" to="/projects">Go to Projects</Link>
+      </div>
+    
+      <div className="resume">
+          <h1>Welcome to my Resume Page</h1>
+            <p>A little bit of me</p>
       
-      <h2>Education</h2>
-      {studiesList}
+          <h2>Education</h2>
+            {studiesList}
 
-      <h2>Work Experience</h2>
-      {laboralExprience}
-
-      <Link to="/" className="right-link">Go to Home</Link>
-      <Link to="/projects" className="left-link">Go to Projects</Link>
-    </div>
-  );
+          <h2>Work Experience</h2>
+            {laboralExprience}
+      </div>
+   </>
+ );
 }
 
 
