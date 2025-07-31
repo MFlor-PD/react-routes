da3887e227bc57e08a07836653495081c80fb014
Flor <mariaflorenciaperezd@gmail.com>
2025-07-31 14:45:25 +0200

    footer

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 function Home() {
   return (
+    <>
     <div className="home">
       <h1>Welcome to my portfolio</h1>
       <p>I'm a very simple person who enjoys life, takes every opportunity it offers, and is always grateful for it.
@@ -11,6 +13,33 @@ function Home() {
       <br />
       <span><Link className="left-link" to="/resume">Go to Resume</Link></span>
     </div>
+    
+    <footer className="contact-icons">
+  <a
+    href="https://github.com/MFlor-PD"
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label="GitHub"
+  >
+    <FaGithub size={30} />
+  </a>
+  <a
+    href="https://www.linkedin.com/in/florenciaperezd"
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label="LinkedIn"
+  >
+    <FaLinkedin size={30} />
+  </a>
+  <a
+    href="mailto:mariaflorenciaperezd@gmail.com"
+    aria-label="Email"
+  >
+    <FaEnvelope size={30} />
+  </a>
+</footer>
+
+  </>
   );
 }
 
