dae8ef56f2b4dbc9ca36e99c224ee23cc90ecf81
Flor <mariaflorenciaperezd@gmail.com>
2025-08-04 12:18:34 +0200

    cambio color, hover, responsive y reorganizar lugares

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,27 @@
 const projects = [
   {
-    id: 1,
-    name: 'Project 1 - Dashboard',
+    id: 3,
+    name: 'SocoShifts: Intelligent Work Roster Tool',
     image:
-      'https://res.cloudinary.com/dn221o8fa/image/upload/v1749924566/Screenshot_2025-06-14_200831_sx2gzd.png',
+      'https://res.cloudinary.com/dn221o8fa/image/upload/v1753382096/Screenshot_2025-07-24_203341_xzz9jf.png',
+    description: `Application designed to manage work rosters in dynamic environments such as hospitality, restaurants, and lifeguard services. It automatically generates work shifts based on specific parameters like daily workload, number of workers, and staffing needs. Currently in MVP stage focused on lifeguard services only.
+  Backend: Node.js, Express, PostgreSQL, AWS, Prisma.
+  Frontend: React and CSS.`,
+    url: 'https://beachrostermanagment.netlify.app',
+  },
+  {
+    id: 2,
+    name: 'E-commerce',
+    image:
+    'https://res.cloudinary.com/dn221o8fa/image/upload/v1749924646/Screenshot_2025-06-14_201032_t8ybro.png',
+    description: `E-commerce application with user authentication and product management. Built with React, Node.js, and MongoDB.`,
+    url: 'https://proyect-break-fullbackend.onrender.com/products',
+  },
+  {
+    id: 1,
+    name: 'Multi-Functional Dashboard',
+    image:
+    'https://res.cloudinary.com/dn221o8fa/image/upload/v1749924566/Screenshot_2025-06-14_200831_sx2gzd.png',
     description: `Multifunctional Dashboard built with HTML, CSS, and JavaScript — a 100% frontend application deployed via GitHub Pages.
 Features include:
 - A password manager
@@ -12,25 +30,7 @@ Features include:
 - A weather app that fetches data from the Weather API using fetch
 - A link organizer that stores and manages URLs using localStorage`,
     url: 'https://mflor-pd.github.io/project-break-dashboard/',
-  },
-  {
-    id: 2,
-    name: 'Project 2 - E-commerce',
-    image:
-      'https://res.cloudinary.com/dn221o8fa/image/upload/v1749924646/Screenshot_2025-06-14_201032_t8ybro.png',
-    description: `E-commerce application with user authentication and product management. Built with React, Node.js, and MongoDB.`,
-    url: 'https://proyect-break-fullbackend.onrender.com/products',
-  },
-  {
-    id: 3,
-    name: 'Project 3 - SocoShifts: Intelligent Work Roster Tool',
-    image:
-      'https://res.cloudinary.com/dn221o8fa/image/upload/v1753382096/Screenshot_2025-07-24_203341_xzz9jf.png',
-    description: `Application designed to manage work rosters in dynamic environments such as hospitality, restaurants, and lifeguard services. It automatically generates work shifts based on specific parameters like daily workload, number of workers, and staffing needs. Currently in MVP stage focused on lifeguard services only.
-Backend: Node.js, Express, PostgreSQL, AWS, Prisma.
-Frontend: React and CSS.`,
-    url: 'https://beachrostermanagment.netlify.app',
-  },
+  }
 ];
 
 export default projects;
